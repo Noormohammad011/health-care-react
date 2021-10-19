9b586f62cb3a61a96ddcb809632dabcfc4ef8ae6
Noormohammad011 <noormohammad.net1503011@gmail.com>
2021-10-19 22:43:51 +0600

    footer is created

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,24 @@ const Footer = () => {
   return (
     <>
       <div className='container-fluid bg-dark text-white'>
-        <div className='container'>
-          
-        </div>
+        
+          <div className='container'>
+            <div className='row row-cols-1 row-cols-md-6'>
+              <div className='col'>
+                <i className='far fa-address-book'>
+                  <span>+01754-867577</span>{' '}
+                </i>
+              </div>
+              <div className='col'>
+                <h1>For Better Inquery</h1>
+                <br />
+                <span>
+                  <i className='far fa-address-book'></i>
+                  <i class='fas fa-envelope-open'></i>
+                </span>
+              </div>
+            </div>
+          </div>
       </div>
     </>
   )
