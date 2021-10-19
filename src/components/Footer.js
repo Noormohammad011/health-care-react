8538e4ae3b5ef024d0415b99163c2f57559eccc0
Noormohammad011 <31389597+Noormohammad011@users.noreply.github.com>
2021-10-19 23:47:28 +0600

    footer is chaged

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,56 @@ const Footer = () => {
   return (
     <>
       <div className='container-fluid bg-dark text-white'>
+<<<<<<< HEAD
+        <div className='container'>
+          <div className='row row-cols-1 row-cols-md-3 d-flex justify-content-between'>
+            <div className='col d-flex justify-content-center align-items-center'>
+              <figure>
+                <blockquote className='blockquote'>
+                  <p>
+                    The art of medicine consists in amusing the patient while
+                    nature cures the disease.
+                  </p>
+                </blockquote>
+                <figcaption className='blockquote-footer'>
+                  Voltire
+                </figcaption>
+              </figure>
+            </div>
+            <div className='col'>
+              <div className='d-flex justify-content-center flex-column'>
+                <h1 className='text-center fontAdd'>
+                  {' '}
+                  <span>For Better Inquery</span>
+                </h1>
+                <div>
+                  <span>
+                    <i className='far fa-address-book fa-2x my-2'>
+                      <span className='mx-2'>Jigatola Dhaka</span>
+                    </i>
+                  </span>
+                </div>
+                <div>
+                  <span>
+                    <i className='fas fa-envelope-open fa-2x my-2'>
+                      <span className='mx-2'>noor@gmail.com</span>
+                    </i>
+                  </span>
+                </div>
+                <div>
+                  <span>
+                    <i className='far fa-address-book fa-2x my-2'>
+                      <span className='mx-2'>+01754-867577</span>{' '}
+                    </i>
+                  </span>
+                </div>
+              </div>
+
+              <br />
+            </div>
+          </div>
+        </div>
+=======
         
           <div className='container'>
             <div className='row row-cols-1 row-cols-md-6'>
@@ -22,6 +72,7 @@ const Footer = () => {
               </div>
             </div>
           </div>
+>>>>>>> 9b586f62cb3a61a96ddcb809632dabcfc4ef8ae6
       </div>
     </>
   )
